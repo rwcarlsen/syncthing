3c0f006934b25e59ef069f4a10755c9464e9dba2
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2015-01-17 19:17:01 +0000

    Receive and store temporary indexes

--- internal/model/model.go
+++ internal/model/model.go
@@ -90,6 +90,8 @@ type Model struct {
 	finder          *db.BlockFinder
 	progressEmitter *ProgressEmitter
 
+	tempIndex *tempIndex
+
 	deviceName    string
 	clientName    string
 	clientVersion string
@@ -149,6 +151,7 @@ func NewModel(cfg *config.Wrapper, deviceName, clientName, clientVersion string,
 		finder:             db.NewBlockFinder(ldb, cfg),
 		progressEmitter:    NewProgressEmitter(cfg),
 		features:           newFeatureSet(),
+		tempIndex:          newTempIndex(),
 	}
 	if cfg.Options().ProgressUpdateIntervalS > -1 {
 		go m.progressEmitter.Serve()
@@ -478,7 +481,11 @@ func (m *Model) NeedFolderFiles(folder string, max int) ([]db.FileInfoTruncated,
 // Index is called when a new device is connected and we receive their full index.
 // Implements the protocol.Model interface.
 func (m *Model) Index(deviceID protocol.DeviceID, folder string, fs []protocol.FileInfo, flags uint32, options []protocol.Option) {
-	if flags != 0 {
+	switch {
+	case flags&protocol.FlagIndexTemporary != 0:
+		m.tempIndex.Update(deviceID, folder, fs)
+		return
+	case flags != 0:
 		l.Warnln("protocol error: unknown flags 0x%x in Index message", flags)
 		return
 	}
@@ -719,6 +726,7 @@ func (m *Model) Close(device protocol.DeviceID, err error) {
 	m.fmut.RLock()
 	for _, folder := range m.deviceFolders[device] {
 		m.folderFiles[folder].Replace(device, nil)
+		m.tempIndex.Update(device, folder, nil)
 	}
 	m.fmut.RUnlock()
 
